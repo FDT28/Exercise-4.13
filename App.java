d75f156ef47525c7543bc8e3c1deb8169807da1f
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 12:11:59 -0500

    commit

--- App.java
+++ App.java
@@ -4,16 +4,14 @@ public class App{
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter an alphabetic letter: ");
         String letter = scanner.nextLine();
-        while ((letter.length()!=1)||
-        (letter.charAt(0)>='A'&&letter.charAt(0)<='Z')||
-        (letter.charAt(0)>='a'&&letter.charAt(0)<='z')) {
+        while ((letter.length()!=1)||(!(Character.isLetter(letter.charAt(0))))) {
             if (letter.length()!=1) {
                 System.out.println("You can only type one letter. Please enter a single letter: ");
                 letter = scanner.nextLine();
             }
-            else if ((letter.charAt(0)>=65&&letter.charAt(0)<=90)||(letter.charAt(0)>=97&&letter.charAt(0)<=122)) {
-            System.out.println("You can only type alphabets. Please enter a single alphabet letter: ");
-            letter = scanner.nextLine();
+            else if (!(Character.isLetter(letter.charAt(0)))) {
+                System.out.println("You can only type alphabets. Please enter a single alphabet letter: ");
+                letter = scanner.nextLine();
             }
 
         }
